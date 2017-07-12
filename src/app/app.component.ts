eccb4b46e44c900b7b60ecbf7f6831b102d55b69
Randell Tuazon <ryt@rescheme.com>
2017-07-12 19:00:24 +0800

    Updated ng4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    profile: any;
-  
-    constructor(public auth: AuthService) {
-        auth.handleAuthentication();
-    }
+
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
+  }
+
 }
