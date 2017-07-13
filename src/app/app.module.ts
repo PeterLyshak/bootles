ea5d11c5d283e3464ccf6cc923f8c7f39ffa0220
Randell Tuazon <ryt@rescheme.com>
2017-07-13 16:15:36 +0800

    Added 404 Error component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { Error404Component } from './errors/error404.component';
 
 import { ROUTES } from './app.routes';
 
@@ -14,17 +15,18 @@ import { ProfileComponent } from './profile/profile.component';
 import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ProfileComponent,
-    CallbackComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
+  ],
+  declarations: [
+    AppComponent,
+    Error404Component,
+    HomeComponent,
+    ProfileComponent,
+    CallbackComponent
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CallbackComponent } from './callback/callback.component';
+// import { DashboardComponent }  from './dashboard/dashboard.component';
+// import { BootstrapComponent }  from './bootstrap/bootstrap-component.component';
+import { Error404Component }  from './errors/error404.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'dashboard', component: Error404Component },
+  { path: 'bootstrap/:id', component: Error404Component },
   { path: 'profile', component: ProfileComponent },
   { path: 'callback', component: CallbackComponent },
   { path: '**', redirectTo: '' }
